c767ea460d09709253ce5bf7f65990a8bd001f28
Jonathan Demaeyer <jodemaey@meteo.be>
2022-10-05 14:22:40 +0200

    Added global training scripts

---
+++ ESSD-mbm-training-global.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Preliminaries
+
+# Timestamping
+
+# In[ ]:
+
+
+import time
+import datetime
+print('Starting training at:')
+print(datetime.datetime.now())
+start_time = time.time()
+print('\n')
+
+
+# Setting the path
+
+# In[ ]:
+
+
+import sys, os
+sys.path.extend([os.path.abspath('./pythie')])
+
+
+# Importing external modules
+
+# In[ ]:
+
+
+import pickle
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# In[ ]:
+
+
+import xarray as xr
+
+
+# Importing internal modules
+
+# In[ ]:
+
+
+from core.data import Data
+import postprocessors.MBM as MBM
+import config
+
+
+# ## Loading the data
+
+# Loading with xarray
+
+# In[ ]:
+
+
+rfcs_data = xr.open_dataarray(config.path_to_data + 'ESSD_benchmark_training_data_forecasts.nc')
+robs_data = xr.open_dataarray(config.path_to_data + 'ESSD_benchmark_training_data_observations.nc')
+
+
+# Transposing to agree with Pythie data model
+
+# In[ ]:
+
+
+rfcs_data = rfcs_data.transpose('time', 'year', 'number', 'step', 'station_id')
+robs_data = robs_data.transpose('time', 'year', 'step', 'station_id')
+
+
+# ## Looping on the seasons and training the Pythie postprocessor objects
+
+# In[ ]:
+
+
+# converting to numpy arrays in °C
+rfcs = rfcs_data.to_numpy() - 273.15
+robs = robs_data.to_numpy() - 273.15
+
+# arranging all the reforecasts of different years along a single axis
+rfcs = rfcs.reshape((rfcs.shape[0]*rfcs.shape[1], rfcs.shape[2], 1, rfcs.shape[3], rfcs.shape[4]))
+robs = robs.reshape((robs.shape[0]*robs.shape[1], 1, 1, robs.shape[2], robs.shape[3]))
+
+# creating the pythie data model
+data_t2_fcs = Data(rfcs[np.newaxis, ..., np.newaxis])
+data_t2_obs = Data(robs[np.newaxis, ..., np.newaxis])
+
+# creating and training a Pythie postprocessor
+postprocessor = MBM.EnsembleAbsCRPSCorrection()
+postprocessor.train(data_t2_obs, data_t2_fcs, ntrial=1)
+
+
+# ## Storing the output
+
+# In[ ]:
+
+
+with open(config.path_to_postprocessors + 'postprocessor-global.pickle', 'wb') as fo:
+    pickle.dump(postprocessor, fo)
+
+
+# ## Plotting the results with the last postprocessor
+
+# Uncomment to check
+
+# In[ ]:
+
+
+# postprocessor.plot_parameters(grid_point=(200,0))
+
+
+# In[ ]:
+
+
+# raw_crps = data_t2_fcs.CRPS(data_t2_obs)
+
+
+# In[ ]:
+
+
+# corr = postprocessor(data_t2_fcs)
+
+
+# In[ ]:
+
+
+# corr_crps = corr.CRPS(data_t2_obs)
+
+
+# In[ ]:
+
+
+# ax = corr_crps.plot(grid_point=(200,0))
+# raw_crps.plot(grid_point=(200,0), ax=ax)
+
+
+# ## Final timing
+
+# In[ ]:
+
+
+print('Training finished at:')
+print(datetime.datetime.now())
+end_time = time.time()
+print('Time elapsed:')
+print(str((end_time - start_time) / 60) + ' minutes elapsed.')
+
